eea9fcfeade1962a2a0598bfa559380342fa3770
Dwight Hendricks <Hendricks.Advisory@gmail.com>
2026-05-08 18:27:16 -0400

    Remove Twitter/X integrations and sharing references

--- adsense-push.js
+++ adsense-push.js
@@ -1,1 +1,6 @@
-(adsbygoogle = window.adsbygoogle || []).push({});
+(function () {
+  var ads = document.querySelectorAll('ins.adsbygoogle:not([data-adsbygoogle-status])');
+  for (var i = 0; i < ads.length; i++) {
+    (window.adsbygoogle = window.adsbygoogle || []).push({});
+  }
+})();
